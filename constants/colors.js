0933b43a937e974187efc6ecd8711b4c27b4276a
Richieal1 <kamuharustau476@gmail.com>
2026-05-17 20:45:48 +0700

    Add source modules

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+export const C = {
+  bg:"#090b12",
+  surface:"#11141e",
+  surface2:"#181b28",
+  border:"#22263a",
+  text:"#dde1f0",
+  muted:"#676b88",
+  accent:"#4f7ef5",
+  green:"#3dd68c",
+  amber:"#f5a623",
+  red:"#f06472",
+  purple:"#a78bfa",
+  cyan:"#38bdf8",
+};
